7a95ab328b6d15d91355e34d2c28e180536a6a93
Omri Mansour <omri.mansour@Omris-MacBook-Pro.local>
2021-09-15 10:12:15 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload...
+		Wael Ya mwle3 you're the next 1
         </p>
         <a
           className="App-link"
